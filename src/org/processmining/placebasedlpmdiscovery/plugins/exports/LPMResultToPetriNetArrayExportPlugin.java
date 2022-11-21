fb5b90bf7049426ef164f0f617ae65fd72b932b2
VikiPeeva <peeva@pads.rwth-aachen.de>
2022-11-21 16:45:12 +0000

    additional exporter added
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/LocalProcessModelDiscoveryByCombiningPlaces/Trunk@46876 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/placebasedlpmdiscovery/plugins/exports/LPMResultToPetriNetArrayExportPlugin.java
@@ -0,0 +1,38 @@
+package org.processmining.placebasedlpmdiscovery.plugins.exports;
+
+
+import org.processmining.acceptingpetrinet.models.impl.AcceptingPetriNetArrayImpl;
+import org.processmining.contexts.uitopia.annotations.UIExportPlugin;
+import org.processmining.framework.plugin.PluginContext;
+import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.annotations.PluginVariant;
+import org.processmining.placebasedlpmdiscovery.model.LocalProcessModel;
+import org.processmining.placebasedlpmdiscovery.model.serializable.LPMResult;
+import org.processmining.placebasedlpmdiscovery.utils.LocalProcessModelUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+@Plugin(
+        name = "Export local process models into an array of Petri nets",
+        returnLabels = {},
+        returnTypes = {},
+        parameterLabels = {"LPMResult", "Filename"})
+@UIExportPlugin(
+        description = "ProM local process models as Accepting Petri Nets",
+        extension = "zip")
+public class LPMResultToPetriNetArrayExportPlugin {
+
+    @PluginVariant(variantLabel = "Export local process models into a file", requiredParameterLabels = {0, 1})
+    public void export(PluginContext context, LPMResult lpmResult, File file) {
+        AcceptingPetriNetArrayImpl lpmPnArray = new AcceptingPetriNetArrayImpl();
+        for (LocalProcessModel lpm : lpmResult.getElements()) {
+            lpmPnArray.addElement(LocalProcessModelUtils.getAcceptingPetriNetRepresentation(lpm));
+        }
+        try {
+            lpmPnArray.exportToFile(context, file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
