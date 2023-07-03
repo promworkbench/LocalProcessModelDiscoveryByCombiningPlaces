7cc7fc0bec2636baca515d5929cd1365335666e5
VikiPeeva <peeva.viki@gmail.com>
2023-07-03 12:58:52 +0200

    merging plugin and zip export

--- src/org/processmining/placebasedlpmdiscovery/plugins/exports/LPMResultToZipWithCsvExportPlugin.java
+++ src/org/processmining/placebasedlpmdiscovery/plugins/exports/LPMResultToZipWithCsvExportPlugin.java
@@ -34,8 +34,50 @@ public class LPMResultToZipWithCsvExportPlugin {
 
     @PluginVariant(variantLabel = "Export local process models into a file", requiredParameterLabels = {0, 1})
     public void export(PluginContext context, LPMResult lpmResult, File file) throws IOException {
+        String fileName = file.getName();
+        String prefix = fileName.substring(0, fileName.indexOf("."));
 
+        ZipOutputStream out = new ZipOutputStream(Files.newOutputStream(file.toPath()));
+
+        String csvFileName = prefix + ".csv";
+        ByteArrayOutputStream csvOS = new ByteArrayOutputStream();
+        CsvWriter csvWriter = new CsvWriter(new OutputStreamWriter(csvOS), ',');
+        csvWriter.writeRecord(new String[]{"Name", "Fitting Windows Score", "Trace Support Score", "Aggregated Score"});
+
+        EvaluationResultAggregateOperation aggregateOperation = new EvaluationResultAggregateOperation();
+        LPMEvaluationResultId[] ids = new LPMEvaluationResultId[]{
+                LPMEvaluationResultId.FittingWindowsEvaluationResult,
+                LPMEvaluationResultId.TraceSupportEvaluationResult
+        };
+
+        for (LocalProcessModel lpm : lpmResult.getElements()) {
+            // convert lpm to accepting petri net
+            AcceptingPetriNet apn = LocalProcessModelUtils.getAcceptingPetriNetRepresentation(lpm);
+
+            String zfName = prefix + "." + lpm.getId() + ".pnml";
+
+            // add the net file to the zip folder
+            ByteArrayOutputStream oos = Utils.exportAcceptingPetriNetToOutputStream(context, apn);
+            addContentToZip(out, oos.toByteArray(), prefix + "." + lpm.getId() + ".pnml");
+
+            // write an entry in the csv
+            csvWriter.write(zfName);
+            for (LPMEvaluationResultId id : ids) {
+                csvWriter.write(String.valueOf(lpm.getAdditionalInfo().getEvaluationResult().getEvaluationResult(id).getResult()));
+            }
+            csvWriter.write(String.valueOf(lpm.getAdditionalInfo().getEvaluationResult().getResult(aggregateOperation)));
+            csvWriter.endRecord();
+        }
+        // add csv file to zip
+        csvWriter.close();
+        addContentToZip(out, csvOS.toByteArray(), csvFileName);
+        out.close();
     }
 
-
+    private void addContentToZip(ZipOutputStream out, byte[] content, String fileName) throws IOException {
+        ZipEntry e = new ZipEntry(fileName.substring(0, fileName.lastIndexOf(".")) + fileName.substring(fileName.lastIndexOf(".")));
+        out.putNextEntry(e);
+        out.write(content);
+        out.closeEntry();
+    }
 }
