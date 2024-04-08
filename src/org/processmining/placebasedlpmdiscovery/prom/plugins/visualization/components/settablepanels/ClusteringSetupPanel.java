201a0b9595f6418c93f57c24b8ecca7ec1083b1d
Viki Peeva <peeva.viki@gmail.com>
2024-04-08 13:13:14 +0200

    change parameter panel when the algorithm selection changes

--- src/org/processmining/placebasedlpmdiscovery/prom/plugins/visualization/components/settablepanels/ClusteringSetupPanel.java
+++ src/org/processmining/placebasedlpmdiscovery/prom/plugins/visualization/components/settablepanels/ClusteringSetupPanel.java
@@ -1,15 +1,18 @@
 package org.processmining.placebasedlpmdiscovery.prom.plugins.visualization.components.settablepanels;
 
 import org.processmining.placebasedlpmdiscovery.grouping.ClusteringAlgorithm;
+import org.processmining.placebasedlpmdiscovery.prom.plugins.visualization.utils.TextFieldIntFilter;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.text.PlainDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Objects;
 
 public class ClusteringSetupPanel extends JPanel {
 
@@ -36,12 +39,71 @@ public class ClusteringSetupPanel extends JPanel {
         this.clustAlgParam = new JPanel();
         this.clustAlgParam.setPreferredSize(new Dimension(200, 100));
         this.clustAlgParam.setBorder(new TitledBorder("Parameters"));
+        setClusteringAlgorithmParametersView((ClusteringAlgorithm) Objects.requireNonNull(clustAlgComboBox.getSelectedItem()));
         this.add(this.clustAlgParam, BorderLayout.CENTER);
     }
 
     private void clusteringAlgorithmChanged(ClusteringAlgorithm algorithm) {
         // remove the parameters panel for the previously selected algorithm
-        this.clustAlgParam.remove(0);
+        if (this.clustAlgParam.getComponents().length > 0) {
+            this.clustAlgParam.remove(0);
+        }
 
+        setClusteringAlgorithmParametersView(algorithm);
+    }
+
+    private void setClusteringAlgorithmParametersView(ClusteringAlgorithm algorithm) {
+        switch (algorithm) {
+            case Hierarchical:
+                this.clustAlgParam.add(getParameterPanelForHierarchicalClust());
+                break;
+            case KMedoids:
+                this.clustAlgParam.add(new JLabel("KMedoids"));
+                break;
+            case DBSCAN:
+                this.clustAlgParam.add(new JLabel("DBSCAN"));
+                break;
+            case Spectral:
+                this.clustAlgParam.add(new JLabel("Spectral"));
+                break;
+            case MinEntropy:
+                this.clustAlgParam.add(new JLabel("MinEntropy"));
+                break;
+        }
+        this.revalidate();
+    }
+
+    private JPanel getParameterPanelForHierarchicalClust() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        // set num clusters or height
+        JPanel numClustersPanel = new JPanel();
+        numClustersPanel.setLayout(new BoxLayout(numClustersPanel, BoxLayout.X_AXIS));
+        JRadioButton btnNumClusters = new JRadioButton("Num Clusters");
+        JRadioButton btnHeight = new JRadioButton("Height");
+        ButtonGroup btnGroup = new ButtonGroup();
+        btnGroup.add(btnNumClusters);
+        btnGroup.add(btnHeight);
+
+        JTextField txtNumClusters = new JTextField();
+        JTextField txtHeight = new JTextField();
+
+        numClustersPanel.add(btnNumClusters);
+        numClustersPanel.add(txtNumClusters);
+        numClustersPanel.add(btnHeight);
+        numClustersPanel.add(txtHeight);
+        panel.add(numClustersPanel);
+
+        // set linkage
+        JPanel linkagePanel = new JPanel();
+        linkagePanel.setLayout(new BoxLayout(linkagePanel, BoxLayout.X_AXIS));
+        linkagePanel.add(new JLabel("Linkage"));
+        linkagePanel.add(Box.createRigidArea(new Dimension(5, 0)));
+        JComboBox<String> linkageComboBox = new JComboBox<>(new String[]{"single", "complete", "wpgma", "upgma"});
+        linkagePanel.add(linkageComboBox);
+        panel.add(linkagePanel);
+
+        return panel;
     }
 }
