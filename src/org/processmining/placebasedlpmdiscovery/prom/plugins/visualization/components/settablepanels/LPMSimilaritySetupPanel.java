d3219ee72f8dc2ea19fdb110a1e8e30153c39e57
Viki Peeva <peeva.viki@gmail.com>
2024-04-09 23:36:03 +0200

    incomplete mixed distance method setup

--- src/org/processmining/placebasedlpmdiscovery/prom/plugins/visualization/components/settablepanels/LPMSimilaritySetupPanel.java
+++ src/org/processmining/placebasedlpmdiscovery/prom/plugins/visualization/components/settablepanels/LPMSimilaritySetupPanel.java
@@ -1,15 +1,11 @@
 package org.processmining.placebasedlpmdiscovery.prom.plugins.visualization.components.settablepanels;
 
 import csplugins.id.mapping.ui.CheckComboBox;
-import org.processmining.framework.util.ui.widgets.ProMCheckBoxWithComboBox;
-import org.processmining.framework.util.ui.widgets.ProMComboCheckBox;
 import org.processmining.placebasedlpmdiscovery.lpmdistances.processmodelsimilarity.ProcessModelSimilarityMeasure;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.util.Objects;
 
@@ -35,7 +31,7 @@ public class LPMSimilaritySetupPanel extends JPanel {
         distMethodPanel.add(distMethodComboBox);
         this.add(distMethodPanel, BorderLayout.PAGE_START);
 
-        // the clustering algorithm parameter panel
+        // the distance method parameter setup panel
         this.distMethodParam = new JPanel();
         this.distMethodParam.setPreferredSize(new Dimension(200, 100));
         this.distMethodParam.setBorder(new TitledBorder("Parameters"));
@@ -57,30 +53,90 @@ public class LPMSimilaritySetupPanel extends JPanel {
 
     private JPanel getDistMethodSetupPanelForMixed() {
         JPanel mixedPanel = new JPanel();
-        mixedPanel.setLayout(new BoxLayout(mixedPanel, BoxLayout.Y_AXIS));
+//        mixedPanel.setLayout(new BoxLayout(mixedPanel, BoxLayout.Y_AXIS));
+        mixedPanel.setLayout(new BorderLayout(0, 5));
+        mixedPanel.setBorder(new TitledBorder("Here bin ich"));
 
         // add additional measure
         JPanel addMeasurePanel = new JPanel();
-        addMeasurePanel.setLayout(new BoxLayout(addMeasurePanel, BoxLayout.X_AXIS));
-        addMeasurePanel.add(new JLabel("Measure"));
+        addMeasurePanel.setLayout(new BoxLayout(addMeasurePanel, BoxLayout.Y_AXIS));
+
+        JPanel chooseMeasurePanel = new JPanel(); // choose measures
+        chooseMeasurePanel.setLayout(new BoxLayout(chooseMeasurePanel, BoxLayout.X_AXIS));
+        chooseMeasurePanel.add(new JLabel("Measure"));
+        chooseMeasurePanel.add(Box.createRigidArea(new Dimension(5, 0)));
         JComboBox<String> measureComboBox = new JComboBox<>(new String[]{"Model Similarity", "Data Attributes"});
-        addMeasurePanel.add(measureComboBox);
-        addMeasurePanel.add(new JLabel("Weight"));
+        chooseMeasurePanel.add(measureComboBox);
+        addMeasurePanel.add(chooseMeasurePanel);
+        addMeasurePanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        JPanel setWeightPanel = new JPanel(); // set weight
+        setWeightPanel.setLayout(new BoxLayout(setWeightPanel, BoxLayout.X_AXIS));
+        setWeightPanel.add(new JLabel("Weight"));
+        setWeightPanel.add(Box.createRigidArea(new Dimension(5, 0)));
         JTextField txtWeight = new JTextField("1");
-        addMeasurePanel.add(txtWeight);
-        JButton btnAddMeasure = new JButton("Add");
-        btnAddMeasure.addActionListener(e -> {
-            // TODO: on click add it in the formula and in the individual measures
-        });
-        addMeasurePanel.add(btnAddMeasure);
-        mixedPanel.add(addMeasurePanel);
+        txtWeight.setMinimumSize(new Dimension(50, measureComboBox.getSize().height));
+        setWeightPanel.add(txtWeight);
+        addMeasurePanel.add(setWeightPanel);
+        addMeasurePanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        JPanel addRemovePanel = new JPanel();
+        addRemovePanel.setLayout(new BoxLayout(addRemovePanel, BoxLayout.X_AXIS));
+        JButton btnAddMeasure = new JButton("Add"); // add measure button
+        addRemovePanel.add(btnAddMeasure);
+        JButton btnRemoveMeasure = new JButton("Remove"); // add measure button
+        addRemovePanel.add(btnRemoveMeasure);
+        addMeasurePanel.add(addRemovePanel);
+        mixedPanel.add(addMeasurePanel, BorderLayout.PAGE_START);
+//        mixedPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+//        // setup of the individual measures
+//        JTabbedPane tabPane = new JTabbedPane();
+//        JScrollPane scrollPane = new JScrollPane(tabPane);
+//        scrollPane.setVisible(false);
+//        mixedPanel.add(scrollPane, BorderLayout.CENTER);
+//
+//        // set action on click
+//        btnAddMeasure.addActionListener(e -> {
+//            if (!scrollPane.isVisible()) {
+//                scrollPane.setVisible(true);
+//            }
+//            String title = "Var " + tabPane.getTabCount();
+//            String measure = (String) Objects.requireNonNull(measureComboBox.getSelectedItem());
+//            tabPane.addTab(title, null,
+//                    getDistMethodParametersView(measure), measure);
+////            setCustomTabFeel(tabPane, title);
+//        });
 
         // show the complete formula
-
-        // setup of the individual measures
-
+        JTextArea txtFormula = new JTextArea();
+//        txtFormula.setPreferredSize(new Dimension(400, 100));
+        mixedPanel.add(txtFormula, BorderLayout.CENTER);
 
         return mixedPanel;
+    }
+
+    private static void setCustomTabFeel(JTabbedPane tabPane, String title) {
+        int index = tabPane.indexOfTab(title);
+        JPanel pnlTab = new JPanel();
+        pnlTab.setLayout(new BoxLayout(pnlTab, BoxLayout.X_AXIS));
+        pnlTab.setOpaque(false);
+        JLabel lblTitle = new JLabel(title);
+        JButton btnClose = new JButton("x");
+        btnClose.setOpaque(false);
+        btnClose.setContentAreaFilled(false);
+        btnClose.setBorderPainted(false);
+
+//        GridBagConstraints gbc = new GridBagConstraints();
+//        gbc.gridx = 0;
+//        gbc.gridy = 0;
+//        gbc.weightx = 1;
+
+        pnlTab.add(lblTitle);
+
+//        gbc.gridx++;
+//        gbc.weightx = 0;
+        pnlTab.add(btnClose);
+
+        tabPane.setTabComponentAt(index, pnlTab);
     }
 
     private JPanel getDistMethodSetupPanelForDataAttributes() {
