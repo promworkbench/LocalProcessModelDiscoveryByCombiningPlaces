8c7ba59e6c1cc61aa324ef327b0a7b4d0452905f
Viki Peeva <peeva.viki@gmail.com>
2024-06-19 09:20:48 +0200

    mixed similarity name and weight

--- src/org/processmining/placebasedlpmdiscovery/prom/plugins/visualization/components/settablepanels/lpmsimilarity/MixedLPMSimilaritySetupPanel.java
+++ src/org/processmining/placebasedlpmdiscovery/prom/plugins/visualization/components/settablepanels/lpmsimilarity/MixedLPMSimilaritySetupPanel.java
@@ -8,6 +8,7 @@ import org.processmining.placebasedlpmdiscovery.prom.plugins.visualization.compo
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.UUID;
 
 public class MixedLPMSimilaritySetupPanel extends JPanel {
 
@@ -49,10 +50,35 @@ public class MixedLPMSimilaritySetupPanel extends JPanel {
 
     private JFrame getAddDistMeasureInMixedFrame() {
         JFrame frame = new JFrame();
+        frame.setTitle("LPM Similarity Measure Setup");
         frame.setSize(new Dimension(500, 350));
+        frame.setLayout(new BorderLayout(5, 5));
 
+        // name and weight
+        JPanel pnlNameAndWeight = new JPanel();
+        pnlNameAndWeight.setLayout(new BorderLayout(5, 5));
+        JPanel pnlName = new JPanel();
+        pnlName.setLayout(new BoxLayout(pnlName, BoxLayout.X_AXIS));
+        pnlName.add(new JLabel("Name:"));
+        JTextField txtName = new JTextField(UUID.randomUUID().toString());
+        pnlName.add(txtName);
+        pnlNameAndWeight.add(pnlName, BorderLayout.LINE_START);
+        JPanel pnlWeight = new JPanel();
+        pnlWeight.setLayout(new BoxLayout(pnlWeight, BoxLayout.X_AXIS));
+        pnlWeight.add(new JLabel("Weight:"));
+        JTextField txtWeight = new JTextField("1");
+        txtWeight.setPreferredSize(new Dimension(150, txtWeight.getPreferredSize().height));
+        pnlWeight.add(txtWeight);
+        pnlNameAndWeight.add(pnlWeight, BorderLayout.LINE_END);
+        frame.add(pnlNameAndWeight, BorderLayout.PAGE_START);
+
+        // similarity selection
         LPMDViewComponent component = this.componentFactory.create(LPMDViewComponentType.LPMSimilarityChooser);
-        frame.add(component.getComponent());
+        frame.add(component.getComponent(), BorderLayout.CENTER);
+
+        // add button
+        JButton btnAdd = new JButton("Add");
+        frame.add(btnAdd, BorderLayout.PAGE_END);
 
         return frame;
     }
