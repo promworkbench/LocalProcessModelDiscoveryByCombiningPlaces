21f3f46530276f45e2035abc0e5fd2f08bdd1f86
Viki Peeva <peeva.viki@gmail.com>
2024-07-02 12:32:02 +0200

    Data Communication Listener for ProM operation - example export

---
+++ src/org/processmining/placebasedlpmdiscovery/view/datacommunication/emittabledata/export/ExportRequestedEmittableDataVM.java
@@ -0,0 +1,21 @@
+package org.processmining.placebasedlpmdiscovery.view.datacommunication.emittabledata.export;
+
+import org.processmining.placebasedlpmdiscovery.view.datacommunication.emittabledata.EmittableDataTypeVM;
+import org.processmining.placebasedlpmdiscovery.view.datacommunication.emittabledata.EmittableDataVM;
+
+public class ExportRequestedEmittableDataVM implements EmittableDataVM {
+
+    private final Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public ExportRequestedEmittableDataVM(Object data) {
+        this.data = data;
+    }
+    @Override
+    public EmittableDataTypeVM getType() {
+        return EmittableDataTypeVM.ExportRequestedVM;
+    }
+}
