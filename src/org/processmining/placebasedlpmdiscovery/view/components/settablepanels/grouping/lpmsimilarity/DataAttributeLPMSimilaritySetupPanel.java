69b55af2c7fc93928967299ec45651a22a51eefc
Viki Peeva <peeva.viki@gmail.com>
2024-07-25 14:43:51 +0200

    update size of data attribute chooser

--- src/org/processmining/placebasedlpmdiscovery/view/components/settablepanels/grouping/lpmsimilarity/DataAttributeLPMSimilaritySetupPanel.java
+++ src/org/processmining/placebasedlpmdiscovery/view/components/settablepanels/grouping/lpmsimilarity/DataAttributeLPMSimilaritySetupPanel.java
@@ -27,9 +27,12 @@ public class DataAttributeLPMSimilaritySetupPanel extends JPanel implements LPMS
         this.eventLogService = eventLogService;
 
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        this.add(new JLabel("Data Attributes:"));
+//        this.setPreferredSize(new Dimension(160, this.getPreferredSize().height));
+        JLabel lbl = new JLabel("Data Attributes:");
+        this.add(lbl);
         this.add(Box.createRigidArea(new Dimension(5, 0)));
         dataAttrComboBox = new CheckComboBox(this.eventLogService.getEventAttributesKeys(), true);
+        dataAttrComboBox.setPreferredSize(new Dimension(160, dataAttrComboBox.getPreferredSize().height));
         this.add(dataAttrComboBox);
     }
 
