2b9a22d1c98872fd304a3999cec943aef898fbd3
Viki Peeva <peeva.viki@gmail.com>
2024-09-09 13:16:00 +0200

    initial setup for Stochastic LPM discovery

--- src/org/processmining/placebasedlpmdiscovery/lpmdiscovery/directors/LPMDiscoveryDirector.java
+++ src/org/processmining/placebasedlpmdiscovery/lpmdiscovery/directors/LPMDiscoveryDirector.java
@@ -1,8 +1,6 @@
 package org.processmining.placebasedlpmdiscovery.lpmdiscovery.directors;
 
-import org.processmining.placebasedlpmdiscovery.lpmdiscovery.algorithms.LPMDiscoveryAlg;
-
 public interface LPMDiscoveryDirector {
 
-    LPMDiscoveryAlg make();
+    void make();
 }
