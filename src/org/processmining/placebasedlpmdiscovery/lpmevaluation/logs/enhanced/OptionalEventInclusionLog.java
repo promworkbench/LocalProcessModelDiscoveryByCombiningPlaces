564f8b7500c3ed5043abe8bf77670b74869461be
Viki Peeva <peeva.viki@gmail.com>
2024-11-19 15:17:34 +0100

    Remove unused enhanced event log classes

--- src/org/processmining/placebasedlpmdiscovery/lpmevaluation/logs/enhanced/OptionalEventInclusionLog.java
+++
@@ -1,8 +0,0 @@
-package org.processmining.placebasedlpmdiscovery.lpmevaluation.logs.enhanced;
-
-import org.deckfour.xes.model.XEvent;
-
-public interface OptionalEventInclusionLog {
-
-    boolean isEventIncluded(XEvent event);
-}
