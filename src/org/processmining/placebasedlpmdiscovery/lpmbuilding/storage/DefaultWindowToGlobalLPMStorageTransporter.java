c96b8ac8beb2a041cf812967f626bac1d7feae3e
Viki Peeva <peeva.viki@gmail.com>
2024-11-28 10:36:20 +0100

    implementing move in the default storage transporter

--- src/org/processmining/placebasedlpmdiscovery/lpmbuilding/storage/DefaultWindowToGlobalLPMStorageTransporter.java
+++ src/org/processmining/placebasedlpmdiscovery/lpmbuilding/storage/DefaultWindowToGlobalLPMStorageTransporter.java
@@ -1,10 +1,16 @@
 package org.processmining.placebasedlpmdiscovery.lpmbuilding.storage;
 
+import org.processmining.placebasedlpmdiscovery.lpmbuilding.storage.traversals.WindowLPMStorageTraversal;
+import org.processmining.placebasedlpmdiscovery.model.LocalProcessModel;
 import org.processmining.placebasedlpmdiscovery.model.lpmstorage.GlobalLPMStorage;
 
 public class DefaultWindowToGlobalLPMStorageTransporter implements WindowToGlobalLPMStorageTransporter{
     @Override
     public void move(WindowLPMStorage windowStorage, GlobalLPMStorage globalStorage) {
-
+        WindowLPMStorageTraversal traversal = WindowLPMStorageTraversal.getInstance(windowStorage);
+        while (traversal.hasNext()) {
+            LocalProcessModel lpm = traversal.next();
+            globalStorage.add(lpm);
+        }
     }
 }
---
+++ src/org/processmining/placebasedlpmdiscovery/lpmbuilding/storage/WindowLPMTreeWrapperAsWindowLPMStorage.java
@@ -0,0 +1,4 @@
+package org.processmining.placebasedlpmdiscovery.lpmbuilding.storage;
+
+public class WindowLPMTreeWrapperAsWindowLPMStorage implements WindowLPMStorage {
+}
