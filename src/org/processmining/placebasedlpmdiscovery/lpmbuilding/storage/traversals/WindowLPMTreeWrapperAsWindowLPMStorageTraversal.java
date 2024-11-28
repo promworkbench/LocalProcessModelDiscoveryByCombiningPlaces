b2499911986bdfe9d030b2f263a105b567d12ea8
Viki Peeva <peeva.viki@gmail.com>
2024-11-28 13:19:33 +0100

    implementing window storage wrapper for WindowLPMTree.

--- src/org/processmining/placebasedlpmdiscovery/lpmbuilding/storage/traversals/WindowLPMTreeWrapperAsWindowLPMStorageTraversal.java
+++ src/org/processmining/placebasedlpmdiscovery/lpmbuilding/storage/traversals/WindowLPMTreeWrapperAsWindowLPMStorageTraversal.java
@@ -1,24 +1,29 @@
 package org.processmining.placebasedlpmdiscovery.lpmbuilding.storage.traversals;
 
-import org.processmining.placebasedlpmdiscovery.lpmbuilding.storage.WindowLPMStorage;
+import org.processmining.placebasedlpmdiscovery.lpmbuilding.storage.WindowLPMTreeWrapperAsWindowLPMStorage;
 import org.processmining.placebasedlpmdiscovery.model.LocalProcessModel;
+
+import java.util.List;
 
 public class WindowLPMTreeWrapperAsWindowLPMStorageTraversal implements WindowLPMStorageTraversal {
 
 
-    private final WindowLPMStorage storage;
+    private final WindowLPMTreeWrapperAsWindowLPMStorage storage;
 
-    public WindowLPMTreeWrapperAsWindowLPMStorageTraversal(WindowLPMStorage storage) {
+    private final List<LocalProcessModel> lpms;
+
+    public WindowLPMTreeWrapperAsWindowLPMStorageTraversal(WindowLPMTreeWrapperAsWindowLPMStorage storage) {
         this.storage = storage;
+        this.lpms = this.storage.getAllValidLPMs();
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return !lpms.isEmpty();
     }
 
     @Override
     public LocalProcessModel next() {
-        return null;
+        return lpms.remove(0);
     }
 }
