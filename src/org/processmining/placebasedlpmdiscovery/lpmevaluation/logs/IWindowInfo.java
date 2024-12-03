e3a49923de9e66f919fa6513e75d48492a15fb89
Viki Peeva <peeva.viki@gmail.com>
2024-12-03 15:33:10 +0100

    populate the window info interface with the important methods and implement in subclasses

--- src/org/processmining/placebasedlpmdiscovery/lpmevaluation/logs/IWindowInfo.java
+++ src/org/processmining/placebasedlpmdiscovery/lpmevaluation/logs/IWindowInfo.java
@@ -1,4 +1,13 @@
 package org.processmining.placebasedlpmdiscovery.lpmevaluation.logs;
 
+import java.util.ArrayList;
+
 public interface IWindowInfo {
+    String getParentSequenceId();
+
+    ArrayList<Integer> getWindow();
+
+    int getStartPos();
+
+    int getEndPos();
 }
