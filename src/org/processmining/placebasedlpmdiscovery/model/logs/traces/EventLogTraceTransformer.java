871f50cd478772adb4c48853492ed2dabec591be
peeva <peeva.viki@gmail.com>
2025-01-08 12:36:13 +0100

    EventLogTraceTransformer implemented for XTraceWrapper

--- src/org/processmining/placebasedlpmdiscovery/model/logs/traces/EventLogTraceTransformer.java
+++ src/org/processmining/placebasedlpmdiscovery/model/logs/traces/EventLogTraceTransformer.java
@@ -4,7 +4,11 @@ import org.apache.commons.lang.NotImplementedException;
 import org.deckfour.xes.model.XTrace;
 
 public interface EventLogTraceTransformer {
-    static XTrace toXTrace(EventLogTrace trace) {
-        throw new NotImplementedException("Not yet implemented");
+    static XTrace toXTrace(EventLogTrace<?> trace) {
+        if (trace instanceof XTraceWrapper) {
+            XTraceWrapper cTrace = (XTraceWrapper) trace;
+            return cTrace.getOriginalTrace();
+        }
+        throw new NotImplementedException("Not yet implemented for " + trace.getClass());
     }
 }
--- src/org/processmining/placebasedlpmdiscovery/model/logs/traces/XTraceWrapper.java
+++ src/org/processmining/placebasedlpmdiscovery/model/logs/traces/XTraceWrapper.java
@@ -6,20 +6,39 @@ import org.processmining.placebasedlpmdiscovery.utils.Constants;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class XTraceWrapper implements EventLogTrace<XEventWrapper> {
 
     private final List<XEventWrapper> events;
+    private final XTrace originalTrace;
 
     public XTraceWrapper(XTrace trace) {
         this.events = Collections.unmodifiableList(trace.stream().map(xEvent -> new XEventWrapper(xEvent,
                 Constants.Logs.DEFAULT_CASE_ATTRIBUTE)).collect(Collectors.toList()));
+        this.originalTrace = trace;
     }
 
     @Override
     public void forEach(Consumer<XEventWrapper> action) {
         events.forEach(action);
+    }
+
+    public XTrace getOriginalTrace() {
+        return originalTrace;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        XTraceWrapper that = (XTraceWrapper) o;
+        return Objects.equals(originalTrace, that.originalTrace);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(originalTrace);
     }
 }
