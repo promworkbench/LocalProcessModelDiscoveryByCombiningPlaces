a9c2ee2c1cf6f54f209a30abce5437be359f5203
peeva <peeva.viki@gmail.com>
2025-09-08 17:06:10 +0200

    Setting a default value for proximity; Obtained updated results from the different runners;

--- src/org/processmining/lpms/discovery/DiscoveryParameters.java
+++ src/org/processmining/lpms/discovery/DiscoveryParameters.java
@@ -2,6 +2,6 @@ package org.processmining.lpms.discovery;
 
 public class DiscoveryParameters {
     public static class Default {
-        public static int proximity;
+        public static int proximity = 7;
     }
 }
