07252ba78aa24725c36d0afb4dba7d5732671f17
peeva <peeva.viki@gmail.com>
2025-10-10 11:17:33 +0200

    restructure PlacesProvider such that it does not force all providers to depend on an event log by replacing `from(XLog log)` with `provide()`. As a consequence `specpp()` and `est()` had to be move to FromLogPlacesProvider

--- src/org/processmining/lpms/discovery/DiscoveryParameters.java
+++ src/org/processmining/lpms/discovery/DiscoveryParameters.java
@@ -1,6 +1,10 @@
 package org.processmining.lpms.discovery;
 
 public class DiscoveryParameters {
+    public static class PlaceBased {
+        public static int placeLimit = 100;
+    }
+
     public static class Default {
         public static int proximity = 7;
     }
