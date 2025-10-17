0c93ac16dc280aa6b7a27303d740ecd72982ad7a
peeva <peeva.viki@gmail.com>
2025-10-17 15:48:10 +0200

    squash-me: add docstrings and rename the variant provider to FromLogPlacesProvider.FromLogPlacesProviderVariantProvider

--- src/org/processmining/placebasedlpmdiscovery/prom/PlacesProvider.java
+++ src/org/processmining/placebasedlpmdiscovery/prom/PlacesProvider.java
@@ -7,16 +7,31 @@ import java.util.Set;
 
 public interface PlacesProvider {
 
+    /**
+     * Creates a PlacesProvider that reads places from a file.
+     * @param fileName the name of the file to read places from
+     * @return a PlacesProvider that reads places from the specified file
+     */
     static PlacesProvider fromFile(String fileName) {
         return new FromFilePlacesProvider(fileName);
     }
 
+    /**
+     * Creates a PlacesProvider that provides the given set of places.
+     * @param places the set of places to provide
+     * @return a PlacesProvider that provides the given set of places
+     */
     static PlacesProvider fromSet(Set<Place> places) {
         return () -> places;
     }
 
-    static FromLogPlacesProvider.FromLogPlacesProviderLogStage fromLog(XLog log) {
-        return new FromLogPlacesProvider.FromLogPlacesProviderLogStage(log);
+    /**
+     * Creates a FromLogPlacesProviderLogStage to configure place discovery from an XLog.
+     * @param log the XLog to discover places from
+     * @return a FromLogPlacesProviderLogStage to configure place discovery
+     */
+    static FromLogPlacesProvider.FromLogPlacesProviderVariantProvider fromLog(XLog log) {
+        return new FromLogPlacesProvider.FromLogPlacesProviderVariantProvider(log);
     }
 
     Set<Place> provide();
