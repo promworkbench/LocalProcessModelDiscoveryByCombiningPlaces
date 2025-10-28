8a76f78bf86f9f3c44772deea322a8f8f5b0b5d5
peeva <peeva.viki@gmail.com>
2025-10-28 08:20:56 +0100

    Replace discovery algorithm builder with one of LPMDiscovery default methods

--- src/org/processmining/placebasedlpmdiscovery/lpmdiscovery/service/DefaultLPMDiscoveryService.java
+++ src/org/processmining/placebasedlpmdiscovery/lpmdiscovery/service/DefaultLPMDiscoveryService.java
@@ -1,15 +1,13 @@
 package org.processmining.placebasedlpmdiscovery.lpmdiscovery.service;
 
 import com.google.inject.Inject;
-import org.processmining.placebasedlpmdiscovery.Main;
 import org.processmining.placebasedlpmdiscovery.datacommunication.DataCommunicationController;
 import org.processmining.placebasedlpmdiscovery.datacommunication.emittabledata.LPMSetDiscoveredEmittableData;
-import org.processmining.placebasedlpmdiscovery.lpmbuilding.inputs.FPGrowthForPlacesLPMBuildingInput;
-import org.processmining.placebasedlpmdiscovery.lpmdiscovery.algorithms.inputs.StandardLPMDiscoveryInput;
-import org.processmining.placebasedlpmdiscovery.lpmdiscovery.algorithms.parameters.PlaceBasedLPMDiscoveryParameters;
+import org.processmining.placebasedlpmdiscovery.lpmdiscovery.LPMDiscovery;
 import org.processmining.placebasedlpmdiscovery.model.discovery.LPMDiscoveryResult;
 import org.processmining.placebasedlpmdiscovery.model.logs.EventLog;
 import org.processmining.placebasedlpmdiscovery.model.serializable.PlaceSet;
+import org.processmining.placebasedlpmdiscovery.prom.PlacesProvider;
 
 public class DefaultLPMDiscoveryService implements LPMDiscoveryService {
 
@@ -22,10 +20,8 @@ public class DefaultLPMDiscoveryService implements LPMDiscoveryService {
 
     @Override
     public LPMDiscoveryResult runLPMDiscovery(EventLog log, PlaceSet placeSet) {
-        PlaceBasedLPMDiscoveryParameters parameters = new PlaceBasedLPMDiscoveryParameters(log);
-        LPMDiscoveryResult result = Main.createDefaultBuilder(log.getOriginalLog(), parameters).build()
-                .run(new StandardLPMDiscoveryInput(log, new FPGrowthForPlacesLPMBuildingInput(log,
-                                placeSet.getPlaces().getPlaces())), parameters);
+        LPMDiscoveryResult result = LPMDiscovery.placeBased(PlacesProvider.fromSet(placeSet.getPlaces().getPlaces()))
+                .from(log.getOriginalLog());
         dc.emit(new LPMSetDiscoveredEmittableData(result));
         return result;
     }
