03c25180cdd8733e7ffc3e1fc0c98771e9038883
peeva <peeva.viki@gmail.com>
2025-11-11 17:41:55 +0100

    squash-me: initial skeleton for building LPMs using PBLADAWindowLPMBuilder

--- src/org/processmining/lpms/discovery/builders/PBLADAWindowLPMBuilder.java
+++ src/org/processmining/lpms/discovery/builders/PBLADAWindowLPMBuilder.java
@@ -1,8 +1,11 @@
 package org.processmining.lpms.discovery.builders;
 
+import org.apache.commons.lang3.NotImplementedException;
+import org.processmining.lpms.model.LPM;
 import org.processmining.placebasedlpmdiscovery.lpmbuilding.storage.WindowLPMStorage;
 import org.processmining.placebasedlpmdiscovery.lpmevaluation.logs.SlidingWindowInfo;
 import org.processmining.placebasedlpmdiscovery.model.Place;
+import org.processmining.placebasedlpmdiscovery.model.logs.activities.Activity;
 import org.processmining.placebasedlpmdiscovery.prom.PlacesProvider;
 
 import java.util.Set;
@@ -17,8 +20,31 @@ public class PBLADAWindowLPMBuilder implements LADAWindowLPMBuilder {
 
     @Override
     public WindowLPMStorage build(SlidingWindowInfo windowInfo, WindowLPMStorage prevWindowResult) {
-//        Activity newAct = windowInfo.getWindow().get(windowInfo.getWindow().size()-1);
-//        prevWindowResult.extend(newAct);
-        return null;
+        WindowLPMStorage newStorage = prevWindowResult;
+        if (windowInfo.getRemovedActivity() != null) {
+            newStorage.removeLPMsFor(windowInfo.getRemovedActivity(), windowInfo.getStartPos() - 1);
+        }
+        if (windowInfo.getAddedActivity() != null) {
+            Activity activity = windowInfo.getAddedActivity();
+            while (newStorage.hasNext()) {
+                LPM lpm = newStorage.next();
+                extendByAddingInitial(lpm, activity);
+                extendByConcatenation(lpm, activity);
+                extendByFiring(lpm, activity);
+            }
+        }
+        return newStorage;
+    }
+
+    private void extendByFiring(LPM lpm, Activity activity) {
+        throw new NotImplementedException();
+    }
+
+    private void extendByConcatenation(LPM lpm, Activity activity) {
+        throw new NotImplementedException();
+    }
+
+    private void extendByAddingInitial(LPM lpm, Activity activity) {
+        throw new NotImplementedException();
     }
 }
