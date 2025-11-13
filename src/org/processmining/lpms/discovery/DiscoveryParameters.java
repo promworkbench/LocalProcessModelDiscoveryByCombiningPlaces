7e2a7b4727300499da2430481e5f2e1293b3f99b
peeva <peeva.viki@gmail.com>
2025-11-13 10:29:37 +0100

    Updating default place limit to 50

--- src/org/processmining/lpms/discovery/DiscoveryParameters.java
+++ src/org/processmining/lpms/discovery/DiscoveryParameters.java
@@ -2,7 +2,7 @@ package org.processmining.lpms.discovery;
 
 public class DiscoveryParameters {
     public static class PlaceBased {
-        public static int placeLimit = 100;
+        public static int placeLimit = 50;
     }
 
     public static class Default {
