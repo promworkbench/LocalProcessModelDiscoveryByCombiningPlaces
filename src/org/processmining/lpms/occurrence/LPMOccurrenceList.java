bf6efad71b2743224553ec61816dcc602f493a74
peeva <peeva.viki@gmail.com>
2026-05-12 14:16:19 +0200

    added LPMOccurrenceList.merge

--- src/org/processmining/lpms/occurrence/LPMOccurrenceList.java
+++ src/org/processmining/lpms/occurrence/LPMOccurrenceList.java
@@ -2,10 +2,21 @@ package org.processmining.lpms.occurrence;
 
 import org.apache.commons.math3.util.Pair;
 
+import java.util.Arrays;
+
 public interface LPMOccurrenceList extends Iterable<Pair<Integer, Integer>> {
 
     static LPMOccurrenceList standard() {
         return new StandardLPMOccurrenceList();
+    }
+
+    static LPMOccurrenceList merge(LPMOccurrenceList... occurrenceLists) {
+        return Arrays.stream(occurrenceLists)
+                .reduce((first, second) -> {
+                    second.forEach(v -> first.push(v.getFirst(), v.getSecond()));
+                    return first;
+                })
+                .orElse(LPMOccurrenceList.standard());
     }
 
     void push(int traceIndex, int eventIndex);
